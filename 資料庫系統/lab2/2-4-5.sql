b5df0ede74e8b8f8b2ae4cbb5741b8df650c59d2
yuean95 <147057181+yuean95@users.noreply.github.com>
2023-10-05 20:56:49 +0800

    Update and rename 2-4-5.sql to 資料庫系統/lab2/2-4-5.sql

---
+++ 資料庫系統/lab2/2-4-5.sql
@@ -0,0 +1,57 @@
+﻿USE [MySchoolDB]
+GO
+/****** Object:  Table [dbo].[Department]    Script Date: 2023/10/5 下午 07:38:52 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Department](
+	[Dep_code] [char](4) NOT NULL,
+	[Dep_name] [nvarchar](10) NOT NULL,
+	[Dep_head] [nvarchar](10) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Dep_code] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Student]    Script Date: 2023/10/5 下午 07:38:52 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Student](
+	[stu_id] [char](5) NOT NULL,
+	[stu_name] [nvarchar](10) NOT NULL,
+	[dep_code] [char](4) NOT NULL,
+	[e-mail] [varchar](1) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[stu_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Student_bk]    Script Date: 2023/10/5 下午 07:38:52 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Student_bk](
+	[stu_id] [char](5) NOT NULL,
+	[stu_name] [nvarchar](10) NOT NULL,
+	[dep_code] [char](4) NOT NULL
+) ON [PRIMARY]
+GO
+INSERT [dbo].[Department] ([Dep_code], [Dep_name], [Dep_head]) VALUES (N'D001', N'資工系', N'李春雄')
+INSERT [dbo].[Department] ([Dep_code], [Dep_name], [Dep_head]) VALUES (N'D002', N'資管系', N'李安')
+GO
+INSERT [dbo].[Student] ([stu_id], [stu_name], [dep_code], [e-mail]) VALUES (N'S0001', N'一心', N'D001', NULL)
+INSERT [dbo].[Student] ([stu_id], [stu_name], [dep_code], [e-mail]) VALUES (N'S0002', N'二聖', N'D001', NULL)
+INSERT [dbo].[Student] ([stu_id], [stu_name], [dep_code], [e-mail]) VALUES (N'S0003', N'三多', N'D002', NULL)
+INSERT [dbo].[Student] ([stu_id], [stu_name], [dep_code], [e-mail]) VALUES (N'S0004', N'四維', N'D002', NULL)
+GO
+INSERT [dbo].[Student_bk] ([stu_id], [stu_name], [dep_code]) VALUES (N'S0001', N'一心', N'D001')
+INSERT [dbo].[Student_bk] ([stu_id], [stu_name], [dep_code]) VALUES (N'S0002', N'二聖', N'D001')
+INSERT [dbo].[Student_bk] ([stu_id], [stu_name], [dep_code]) VALUES (N'S0003', N'三多', N'D002')
+INSERT [dbo].[Student_bk] ([stu_id], [stu_name], [dep_code]) VALUES (N'S0004', N'四維', N'D002')
+GO
