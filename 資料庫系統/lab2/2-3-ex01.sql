b19e0efd50ece14ef965f76cc4f3a5c82602a4f9
yuean95 <147057181+yuean95@users.noreply.github.com>
2023-10-05 20:49:20 +0800

    Update and rename 2-3-ex01.sql to 資料庫系統/lab2/2-3-ex01.sql

---
+++ 資料庫系統/lab2/2-3-ex01.sql
@@ -0,0 +1,32 @@
+﻿USE [MySchoolDB]
+GO
+/****** Object:  Table [dbo].[Department]    Script Date: 2023/10/5 下午 06:40:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Department](
+	[Dep_code] [char](4) NOT NULL,
+	[Dep_name] [nvarchar](10) NOT NULL,
+	[Dep_head] [nvarchar](10) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Dep_code] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Student]    Script Date: 2023/10/5 下午 06:40:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Student](
+	[stu_id] [char](5) NOT NULL,
+	[stu_name] [nvarchar](10) NOT NULL,
+	[dep_code] [char](4) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[stu_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
