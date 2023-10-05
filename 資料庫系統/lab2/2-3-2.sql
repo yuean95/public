1edc2b716bc4910e09263733f5fc32f28a64cd3f
yuean95 <147057181+yuean95@users.noreply.github.com>
2023-10-05 20:48:49 +0800

    Update and rename 2-3-2.sql to 資料庫系統/lab2/2-3-2.sql

---
+++ 資料庫系統/lab2/2-3-2.sql
@@ -0,0 +1,17 @@
+﻿USE [MySchoolDB]
+GO
+/****** Object:  Table [dbo].[Department]    Script Date: 2023/10/5 下午 06:34:14 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Department](
+	[Dep_code] [char](4) NOT NULL,
+	[Dep_name] [nvarchar](10) NOT NULL,
+	[Dep_head] [nvarchar](10) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Dep_code] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
