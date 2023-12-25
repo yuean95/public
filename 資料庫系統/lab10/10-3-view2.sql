01df85f1da0895de5d40702c2fb3124273f79031
yuean95 <147057181+yuean95@users.noreply.github.com>
2023-12-25 14:28:22 +0800

    Update 10-3-view2.sql

--- 資料庫系統/lab10/10-3-view2.sql
+++ 資料庫系統/lab10/10-3-view2.sql
@@ -1,5 +1,7 @@
 use [Ch10DB]
 go
 CREATE VIEW Em_view2
-AS SELECT*
-FROM [dbo].[Employee]
+AS
+  SELECT A.Em_id,Em_name,Em_code
+  FROM [dbo].[Employee] AS A,[dbo].[Comp_dep] AS B
+  WHERE A.Em_code=B.Comp_dep_code
