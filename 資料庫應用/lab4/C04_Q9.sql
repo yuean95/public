2117eb24f93c66e4ffff53d0c1399e329996657c
yuean95 <147057181+yuean95@users.noreply.github.com>
2024-04-12 23:33:09 +0800

    Create C04_Q9.sql

---
+++ 資料庫應用/lab4/C04_Q9.sql
@@ -0,0 +1,10 @@
+INSERT INTO shohincopy(
+    shohin_id,
+    shohin_name,
+    shohin_catalg,
+    sell_price,
+    buying_price,
+    reg_date
+)
+SELECT shohin_id,shohin_name,shohin_catalg,sell_price,buying_price,reg_date
+FROM shohinins;
