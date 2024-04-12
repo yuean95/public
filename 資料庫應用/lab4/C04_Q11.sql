beca1218bfa44f59780e70e4fddef49eb80cb565
yuean95 <147057181+yuean95@users.noreply.github.com>
2024-04-12 23:55:23 +0800

    Create C04_Q11.sql

---
+++ 資料庫應用/lab4/C04_Q11.sql
@@ -0,0 +1,8 @@
+INSERT INTO shohincatalg(
+    shohin_catalg,
+    sum_sell_price,
+    sum_buying_price
+)
+SELECT shohin_catalg,SUM(sell_price),SUM(buying_price)
+FROM shohinins
+GROUP BY shohin_catalg;
